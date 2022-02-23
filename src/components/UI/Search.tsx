80a86cb6f0ca7bd5c3ab0000126989278b46c714
Maksim Smetanin <maks.s923@mail.ru>
2022-02-23 16:52:23 +0500

    implements base functional search component

--- src/components/UI/Search.tsx
+++ src/components/UI/Search.tsx
@@ -1,7 +1,35 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { BiSearch } from 'react-icons/bi'
 
-const Search:FC = () => (
-  <div />
-);
+interface SearchProps {
+  changeHandler: (query: string) => void
+}
+
+const Search: FC<SearchProps> = ({ changeHandler }) => {
+  const [query, setQuery] = useState('')
+
+  const changeQuery = (newQuery: string) => {
+    setQuery(newQuery)
+    changeHandler(newQuery)
+  }
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <div className="relative inline-block">
+        <input
+          type="search"
+          value={query}
+          placeholder="Поиск"
+          className="py-1 px-2 font-normal rounded-md border-2 border-gray-400 outline-none w-[300px] pr-8 focus:border-black"
+          onChange={(e) => changeQuery(e.target.value)}
+        />
+        <BiSearch
+          size={25}
+          className="absolute abs-y-center right-1 "
+        />
+      </div>
+    </form>
+  )
+};
 
 export default Search;
--- src/pages/ListLeaguesPage.tsx
+++ src/pages/ListLeaguesPage.tsx
@@ -1,9 +1,17 @@
 import React, { FC } from 'react';
+import { Search } from '../components/UI';
 
-const ListLeaguesPage:FC = () => (
-  <div>
-    <h1>League list page</h1>
-  </div>
-);
+const ListLeaguesPage: FC = () => {
+  const handlerChangeQuery = (query: string) => {
+    console.log(query)
+  }
+
+  return (
+    <main className="container">
+      <Search changeHandler={handlerChangeQuery} />
+      <h1>League list page</h1>
+    </main>
+  )
+};
 
 export default ListLeaguesPage;
